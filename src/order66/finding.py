0be828562dde92a34324a3c5d3f31e135cc41ac0
namita <namitab1604@gmail.com>
2026-08-11 11:14:48 +0530

    Find/Observe unusual activity.

--- src/order66/finding.py
+++ src/order66/finding.py
@@ -1,15 +1,18 @@
-#For finding potentially interesting activity
+# For finding potentially interesting activity
 from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
 
-#define severity classes
+
+# define severity classes
 class Severity(str, Enum):
     LOW = "low"
     MEDIUM = "medium"
     HIGH = "high"
     CRITICAL = "critical"
-#define activity observed
+
+
+# define activity observed
 @dataclass
 class Finding:
     rule_id: str
