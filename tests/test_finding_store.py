166a21377324574da1e00544ac7b89d811c91096
namita <namitab1604@gmail.com>
2026-08-13 09:36:52 +0530

    Added storage for storing findings

---
+++ tests/test_finding_store.py
@@ -0,0 +1,17 @@
+"""Test 1
+Can we save a finding?
+
+Test 2
+Does the file actually get created?
+
+Test 3
+Does the saved JSON contain the correct information?
+
+Test 4
+Can multiple findings be stored?
+
+Test 5
+What happens if the file doesn't exist yet?
+
+Test 6
+What happens if there are no findings?"""
